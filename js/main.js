0f15c298819f39f8e5d2ae5f7f6b0e3b20d995ea
Anne Homann <an.hmnn@gmail.com>
2014-08-28 09:40:44 +1000

    made nav bar interactive, fixed portfolio thumbnails

--- js/main.js
+++ js/main.js
@@ -6,7 +6,11 @@ var App = {
 	functions: {
 		init: function () {
 			console.log("App Initiated.");
+			this.addEventHandlers();
 			this.setUpHeroSlider();
+		},
+		addEventHandlers: function () {
+			$(window).scroll(this.sortOutNav);
 		},
 		setUpHeroSlider: function () {
 			console.log("Set Up Hero Slider called");
@@ -19,6 +23,27 @@ var App = {
 				slide: ".slide",
 				cssEase: 'linear'
 			});
+		}, 
+		sortOutNav: function () {
+			var toShowNav = $("div.title h1").position().top,
+			scrolledDown = $(window).scrollTop(); 
+			if ( scrolledDown >= toShowNav ) {
+				if ( $("nav.non-static").attr("data-scrolled") == "true" ) {
+					$("nav.non-static").addClass("scrolled");
+					$("nav.non-static").animate({
+						top: "0"
+					}, 400);
+					$("nav").attr("data-scrolled", "false");
+				}
+			} else {
+				if ( $("nav.non-static").attr("data-scrolled") == "false" ) {
+					console.log("yesss");
+					$("nav.non-static").attr("data-scrolled", "true");
+					$("nav.non-static").animate({
+						top: "-100px"
+					}, 400);
+				}
+			}
 		}
 	}
 };
